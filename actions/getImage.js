f70f92ec7bb04d59e18de31a7a8868fcaea12f26
Chad Robinson <crobinson@medialantern.com>
2017-03-06 15:05:08 -0700

    getImage endpoint and test.

--- actions/getImage.js
+++ actions/getImage.js
@@ -4,19 +4,20 @@ exports.action = {
     blockedConnectionTypes: ['websocket'],
     inputs: {
         url: { required: true },
-        variant: { required: true },
+        variantId: { required: true },
     },
     run: function(api, data, next) {
         let connection = data.connection,
             server = api.servers.servers[connection.type];
 
-        api.image.convert(data.params.url, data.params.variant).then(path => {
+        api.image.convert(data.params.url, data.params.variantId).then(path => {
             connection.params.file = path;
             server.processFile(connection);
             // NOTE: No next() callback is needed here because processFile destroys the connection.
 
         }).catch(e => {
-            api.log(['Error processing `%s` variant `%s`', data.params.url, data.params.variant], 'error', e);
+            // This is the primary end-developer endpoint so we're a little more friendly here with errors
+            api.log(['Error processing `%s` variant `%s`', data.params.url, data.params.variantId], 'error', e);
             connection.error = 'Error processing request.';
             next(connection, true);
 
---
+++ test/actions/getImage.js
@@ -0,0 +1,25 @@
+const request = require('request'),
+    testData = require('../testData.json');
+
+describe('Action: getImage', () => {
+    before(() => api.models.Variant.truncate()
+        .then(() => api.models.Variant.create(testData.variant)));
+
+    it('Properly processes image requests', done => {
+        let url = [
+            'http:/',
+            ['localhost', api.config.servers.web.port].join(':'),
+            api.config.servers.web.urlPathForActions,
+            'image',
+            testData.variant.id,
+            encodeURIComponent(testData.url)
+        ].join('/');
+
+        request(url, function(error, response, body) {
+            expect(error).to.be.null();
+            expect(response.headers['content-type']).to.be.equal('image/jpeg');
+            expect(body.length).to.be.above(3000);
+            done();
+        });
+    });
+});
